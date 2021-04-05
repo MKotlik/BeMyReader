0114ad32197f86af826be1fd7ef3a2345989fa98
TimManinger <timothy.maninger@tufts.edu>
2021-04-04 22:17:12 -0500

    added 3rd book
    added 3rd book and rebuild db

--- ivr/browse/create_data.py
+++ ivr/browse/create_data.py
@@ -3,15 +3,22 @@ from django.db import models
 from django.conf import settings
 
 Title.objects.create(
-    name="count zero",
+    name="neuromancer",
     author="william gibson",
     genre='LI',
     files=settings.MEDIA_URL+'0/'
 )
 
 Title.objects.create(
+    name="count zero",
+    author="william gibson",
+    genre='LI',
+    files=settings.MEDIA_URL+'1/'
+)
+
+Title.objects.create(
     name="mona lisa overdrive",
     author="william gibson",
     genre="LI",
-    files=settings.MEDIA_URL+'1/'
+    files=settings.MEDIA_URL+'2/'
 )
